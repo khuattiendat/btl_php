c37a8e337a30fbb1a8fc66da729adb5bfd8adabc
khuattiendat <khuattiendat2002@gmail.com>
2023-12-01 12:46:25 +0700

    first commit

---
+++ view/layout/footer.php
@@ -0,0 +1,60 @@
+<div class="footer">
+    <div class="footer-container">
+        <div class="footer__head">
+            <div class="footer__logo">
+                <img src="assets/img/logo.png" alt="" style="max-width: 100px" class="footer__logo-img" />
+            </div>
+            <div class="footer__block">
+                <div class="footer__block-icon">
+                    <i class="fas fa-home"></i>
+                </div>
+                <div class="footer__block__body">
+                    <span class="footer__name">Địa chỉ: Số 209 Xã Đàn, Nam Đồng, Đống Đa, Hà Nội</span>
+                </div>
+            </div>
+            <div class="footer__block">
+                <div class="footer__block-icon">
+                    <i class="fas fa-envelope"></i>
+                </div>
+                <div class="footer__block__body">
+                    Email: <a href="mailto:khuattiendat2002@gmail.com" class="footer__value">khuattiendat2002@gmail.com</a>
+                </div>
+            </div>
+            <div class="footer__block">
+                <div class="footer__block-icon">
+                    <i class="fas fa-mobile-alt"></i>
+                </div>
+                <div class="footer__block__body">
+                    <span class="footer__name">Điện thoại:</span>
+                    <a href="tel:0383878902" class="footer__value">0383878902</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="payment">
+    <div class="payment__logo">
+        <img src="./assets/img/payment-icon.png" style="max-width: 100%; max-height: 40px" alt="" />
+    </div>
+    <ul class="payment__list">
+        <li class="payment__item">
+            <a href="#" class="payment__item-link">Trang chủ</a>
+        </li>
+        <li class="payment__item">
+            <a href="#" class="payment__item-link">giới thiệu</a>
+        </li>
+        <li class="payment__item">
+            <a href="#" class="payment__item-link">sản phẩm</a>
+        </li>
+        <li class="payment__item">
+            <a href="#" class="payment__item-link">tin tức </a>
+        </li>
+        <li class="payment__item" style="border-right: none">
+            <a href="#" class="payment__item-link">liên hệ</a>
+        </li>
+    </ul>
+    <div class="payment__license">
+        © Bản quyền thuộc về khuattiendat |
+        <a href="#" class="NTTA">NTTA</a>
+    </div>
+</div>
